7708f0dd779734649de90d55e572f7ccb3d964fe
JagrutJadhav <64086840+JagrutJadhav@users.noreply.github.com>
2020-05-24 21:26:46 +0530

    Update i2c_master.v

--- i2c_master.v
+++ i2c_master.v
@@ -9,7 +9,7 @@ module i2c_master #(parameter clkfreq=10)(
   reg [15:0] count ;
   reg [7:0]send;
   reg ack,dout;
-  wire rw;
+  reg rw = 1'b0;
   reg [1:0] trig = 2'b00;
   reg [5:0] address = 6'b111001; ////address of slave
   reg [7:0] data = 8'b11001010; ///testing data
@@ -36,25 +36,31 @@ module i2c_master #(parameter clkfreq=10)(
  
  
  always @ (posedge sclk) begin:datastart    
+   ack <= 1'b1;
    if (enable) begin
-   //dout <= 1'b1;
+   dout <= 1'b1;
    
     case(trig)
     2'b00: begin
+           
             dout <= concatadd[send];
-            send<=send+1;
+            send <=send+1;
             
             if (send>=8'd8) begin
-            //$display ("address  = %b", concatadd);
+            $display ("address  = %b", concatadd);
                  trig <= 2'b01;
-                 dout <= 1'b1;
+                 dout <= 1'bz;
+                 rw <=1'b1;
                  send <= 8'd0;
              end
             else
                 trig <= 2'b00;
            end
+           
     2'b01: begin
-            ack <= sda;
+            //ack <= sda;
+            
+            $display("ack = %d",ack);
                 if (ack == 1'b0) begin
                    $display ("welsome to data receieveing case");
                    ack <= 1'b1;
@@ -62,8 +68,10 @@ module i2c_master #(parameter clkfreq=10)(
                 end 
                 else trig <= 2'b01;
            end
+          
+          
     2'b10:  begin
-            if(rw == 1'b0) begin
+            if(readwrite == 1'b0) begin
             dout <= data[send];
             $display ("data  = %b", dout);
             send=send+1;
@@ -78,6 +86,7 @@ module i2c_master #(parameter clkfreq=10)(
           
            else begin 
             readdata[send] <= sda;
+            //$display ("data  = %b", dout);
             send = send+1;
              if (send==8'd8) begin
                  trig <= 2'b00;
@@ -91,7 +100,7 @@ module i2c_master #(parameter clkfreq=10)(
            end
            end
     2'b11: begin
-             ack <= sda;
+             //ack <= sda;
                 if (ack == 1'b0) begin
                    dout<= stopbit;
                    ack <= 1'b1;
@@ -103,12 +112,16 @@ module i2c_master #(parameter clkfreq=10)(
            end 
     default: dout<= 1'b1;
    endcase
+   $display("trig =  %b",trig[1:0]);
    end
 end
-assign sda = (trig == 2'b01 || trig == 2'b11)?ack:dout;
-//assign sda = dout;
-always @ (posedge sclk)begin
+
+/*always @ (posedge sclk)begin
 ack <= sda;
-//$monitor ("%b",ack);
-end
+//$display("ack = %d",ack);
+end*/
+
+assign sda = (trig == 2'b01 || trig == 2'b11)?1'b1:dout;
+//assign sda = dout;
+
 endmodule
